3a227da668722a733032cdcc29e532e97ddc68de
Sai Chandan Mohanty <saichandan68@gmail.com>
2024-02-05 23:51:13 +0530

    Remove validation check for post ID in delete endpoint

--- index.js
+++ index.js
@@ -55,10 +55,6 @@ app.post("/posts", async (req, res) => {
 
 app.delete("/posts/:id", async (req, res) => {
   const { id } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send("No post with that id");
-
   await PostMessage.findByIdAndRemove(id);
 
   res.json({ message: "Post deleted successfully" });
