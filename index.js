71f051c9c12d9bf22d1466a813835e38ae8a5340
Sai Chandan Mohanty <saichandan68@gmail.com>
2024-02-05 22:19:51 +0530

    Add CORS configuration to allow all origins

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const CONNECTION_URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 5000;
